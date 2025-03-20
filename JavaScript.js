852a60138347638dbe5b272a6bf606afb7b7e727
IrisFrades <a24irifragra@inspedralbes.cat>
2025-03-20 20:24:13 +0100

    Cambios

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,23 @@
+let dom = $(document);
+const $Jugador1 = document.getElementById("Jugador1");
 
+let x = 0;
+
+document.addEventListener("keydown", (e)=>{
+    console.log(e.code);
+    if(e.code === "ArrowLeft"){
+        x=x-100;
+        $Jugador1.style.left = x + "px";    
+    }
+
+    if(e.code === "ArrowRight"){
+        x=x+100;
+        $Jugador1.style.left = x + "px";    
+    }
+
+});
+
+/*var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var img = document.getElementById("Jugador1");
+ctx.drawImage(img, 10, 10);*/
