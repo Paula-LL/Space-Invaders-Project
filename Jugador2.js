1e4a54ca5411fc895223c7f13ce8de9fff54e592
Paula-LL <plizelucea@gmail.com>
2025-03-20 20:20:24 +0100

    Movimiento Jugador 2

--- Jugador2.js
+++ Jugador2.js
@@ -1,1 +1,16 @@
 let dom = $(document);
+const $Jugador2 = document.getElementById("Jugador2");
+let x = 0;
+
+document.addEventListener("keydown", (e) => {
+    console.log(e.code);
+    if (e.code === "ArrowLeft") {
+        x = x - 100;
+        $Jugador2.style.right  = (-x) + "px";
+    }
+
+    if (e.code === "ArrowRight") {
+        x = x + 100;
+        $Jugador2.style.right = (-x) + "px";
+    }
+});
